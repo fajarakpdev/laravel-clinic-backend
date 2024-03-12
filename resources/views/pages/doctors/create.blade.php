aab3fdad055036f1b343d97ed5227345b247262a
fajar <fajar@MacBook-Pro-fajar.local>
2024-03-12 20:26:31 +0700

    menambahkan create doctor

---
+++ resources/views/pages/doctors/create.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.app')
+
+@section('title', 'Advanced Forms')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/select2/dist/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Advanced Forms</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Forms</a></div>
+                    <div class="breadcrumb-item">Doctors</div>
+                </div>
+            </div>
+
+            <div class="section-body">
+                <h2 class="section-title">Doctors</h2>
+
+
+
+                <div class="card">
+                    <form action="{{ route('doctors.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-header">
+                            <h4>Input Text</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Doctor Name</label>
+                                <input type="text"
+                                    class="form-control @error('doctor_name')
+                                    is-invalid
+                                    @enderror"
+                                    name="doctor_name">
+                                @error('doctor_name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Doctor Specialist</label>
+                                <input type="text"
+                                    class="form-control @error('doctor_specialist')
+                                    is-invalid
+                                    @enderror"
+                                    name="doctor_specialist">
+                                @error('doctor_specialist')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Doctor Phone</label>
+                                <input type="number" class="form-control" name="doctor_phone">
+                            </div>
+                            <div class="form-group">
+                                <label>Doctor Email</label>
+                                <input type="text"
+                                    class="form-control @error('doctor_email')
+                                    is-invalid
+                                    @enderror"
+                                    name="doctor_email">
+                                @error('doctor_email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Photo</label>
+                                <input type="text"
+                                    class="form-control @error('photo')
+                                    is-invalid
+                                    @enderror"
+                                    name="photo">
+                                @error('photo')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>SIP</label>
+                                <input type="number" class="form-control" name="sip">
+                            </div>
+
+
+                        </div>
+                        <div class="card-footer text-right">
+                            <button class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/cleave.js/dist/cleave.min.js') }}"></script>
+    <script src="{{ asset('library/cleave.js/dist/addons/cleave-phone.us.js') }}"></script>
+    <script src="{{ asset('library/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+    <script src="{{ asset('library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js') }}"></script>
+    <script src="{{ asset('library/bootstrap-timepicker/js/bootstrap-timepicker.min.js') }}"></script>
+    <script src="{{ asset('library/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js') }}"></script>
+    <script src="{{ asset('library/select2/dist/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/forms-advanced-forms.js') }}"></script>
+@endpush
